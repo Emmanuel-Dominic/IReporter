d899c16cc492840a59d97010c65d673dcb73f1c9
Manuel Dominic <ematembu2@gmail.com>
2018-11-22 15:38:22 +0300

    Update pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-var map = L.map('map').setView([37.75,-122.45], 12);
+//onclick order
+function signup() {
+   alert ("Successfully Registered!! You can Now Login");
+}
 
-L.esri.basemapLayer("Topographic").addTo(map);
+//onclick Delete.
+function Delete() {
+  obj.style.backgroundColor="red";
+  obj.innerHTML="Deleted";
+}
+
+//map function
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(0.342500,32.588889),
+    zoom:10,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
